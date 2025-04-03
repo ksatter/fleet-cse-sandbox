1b808ad660f12356e43901160910cf3f960252a1
Katheryn Satterlee <kathy@fleetdm.com>
2025-04-03 12:36:01 -0500

    Update gitops.sh

--- gitops.sh
+++ gitops.sh
@@ -14,31 +14,28 @@ FLEET_DELETE_OTHER_TEAMS="${FLEET_DELETE_OTHER_TEAMS:-true}"
 
 FLEET_MDM_SSO_METADATA=$( sed '2,$s/^/        /' <<<  "${FLEET_MDM_SSO_METADATA}")
 
-echo "$FLEET_MDM_SSO_METADATA"
+# Validate that global file contains org_settings
+grep -Exq "^org_settings:.*" "$FLEET_GLOBAL_FILE"
 
-cat default.yml
-# # Validate that global file contains org_settings
-# grep -Exq "^org_settings:.*" "$FLEET_GLOBAL_FILE"
+if compgen -G "$FLEET_GITOPS_DIR"/teams/*.yml > /dev/null; then
+  # Validate that every team has a unique name.
+  # This is a limited check that assumes all team files contain the phrase: `name: <team_name>`
+  ! perl -nle 'print $1 if /^name:\s*(.+)$/' "$FLEET_GITOPS_DIR"/teams/*.yml | sort | uniq -d | grep . -cq
+fi
 
-# if compgen -G "$FLEET_GITOPS_DIR"/teams/*.yml > /dev/null; then
-#   # Validate that every team has a unique name.
-#   # This is a limited check that assumes all team files contain the phrase: `name: <team_name>`
-#   ! perl -nle 'print $1 if /^name:\s*(.+)$/' "$FLEET_GITOPS_DIR"/teams/*.yml | sort | uniq -d | grep . -cq
-# fi
+args=(-f "$FLEET_GLOBAL_FILE")
+for team_file in "$FLEET_GITOPS_DIR"/teams/*.yml; do
+  args+=(-f "$team_file")
+done
+if [ "$FLEET_DELETE_OTHER_TEAMS" = true ]; then
+  args+=(--delete-other-teams)
+fi
 
-# args=(-f "$FLEET_GLOBAL_FILE")
-# for team_file in "$FLEET_GITOPS_DIR"/teams/*.yml; do
-#   args+=(-f "$team_file")
-# done
-# if [ "$FLEET_DELETE_OTHER_TEAMS" = true ]; then
-#   args+=(--delete-other-teams)
-# fi
+# Dry run
+$FLEETCTL gitops "${args[@]}" --dry-run
+if [ "$FLEET_DRY_RUN_ONLY" = true ]; then
+  exit 0
+fi
 
-# # Dry run
-# $FLEETCTL gitops "${args[@]}" --dry-run
-# if [ "$FLEET_DRY_RUN_ONLY" = true ]; then
-#   exit 0
-# fi
-
-# # Real run
-# $FLEETCTL gitops "${args[@]}"
+# Real run
+$FLEETCTL gitops "${args[@]}"
